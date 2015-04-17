f06f0a98ddc52cd9fd3d2969dabdcaba4978c83b
Martin Urban <martin.urban@firma.seznam.cz>
2015-04-17 12:30:37 +0200

    #! Feed example updated

--- packages/core/examples/feed/page/home/home.view.jsx
+++ packages/core/examples/feed/page/home/home.view.jsx
@@ -17,22 +17,23 @@ ns.namespace('App.Page.Home');
 class View extends ns.Core.Abstract.View {
 
 	/*
-	* @method constructor
-	* @constructor
-	* @param {Vendor.React} React
-	* */
-	constructor(React) {
-		super(React);
+	 * @method constructor
+	 * @constructor
+	 * @param {Vendor.React} React
+	 * @param {Object} utils
+	 */
+	constructor(React, utils) {
+		super(React, utils);
 	}
 
 	/**
 	 * Initialization view.
 	 *
 	 * @method init
-	 * @param {App.Page.Home.Controller} cotroller
-	 * */
-	init(controller) {
-		super.init(controller);
+	 * @param {function()} getInitializationState
+	 */
+	init(getInitializationState) {
+		super.init(getInitializationState);
 		var self = this;
 
 		this._view = this._React.createClass({
