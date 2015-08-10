07e50208130e6e2b83df7582f9da7854204cdf66
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2015-08-10 16:15:51 +0200

    #! refactoring

--- packages/core/examples/helloWorld/component/document/view.jsx
+++ packages/core/examples/helloWorld/component/document/view.jsx
@@ -18,26 +18,26 @@ class View extends ns.Core.Abstract.Component {
 		return (
 			<html>
 				<head>
-					<meta charSet="utf-8" />
-					<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+					<meta charSet="utf-8"/>
+					<meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
 
-					<meta name="description" content={this.props.metaManager.getMetaName('description')} />
-					<meta name="keywords" content={this.props.metaManager.getMetaName('keywords')} />
+					<meta name="description" content={this.props.metaManager.getMetaName('description')}/>
+					<meta name="keywords" content={this.props.metaManager.getMetaName('keywords')}/>
 
-					<meta property="og:title" content={this.props.metaManager.getMetaProperty('og:title')} />
-					<meta property="og:description" content={this.props.metaManager.getMetaProperty('og:description')} />
-					<meta property="og:type" content={this.props.metaManager.getMetaProperty('og:type')} />
-					<meta property="og:url" content={this.props.metaManager.getMetaProperty('og:url')} />
-					<meta property="og:image" content={this.props.metaManager.getMetaProperty('og:image')} />
+					<meta property="og:title" content={this.props.metaManager.getMetaProperty('og:title')}/>
+					<meta property="og:description" content={this.props.metaManager.getMetaProperty('og:description')}/>
+					<meta property="og:type" content={this.props.metaManager.getMetaProperty('og:type')}/>
+					<meta property="og:url" content={this.props.metaManager.getMetaProperty('og:url')}/>
+					<meta property="og:image" content={this.props.metaManager.getMetaProperty('og:image')}/>
 
-					<meta name="viewport" content="width=device-width, initial-scale=1" />
-					<link rel="stylesheet" href={`/static/css/${appCssFile}`} />
+					<meta name="viewport" content="width=device-width, initial-scale=1"/>
+					<link rel="stylesheet" href={`/static/css/${appCssFile}`}/>
 					<title>
 						{this.props.metaManager.getTitle()}
 					</title>
 				</head>
 				<body>
-					<div id="page" dangerouslySetInnerHTML={{ __html: this.props.page }} />
+					<div id="page" dangerouslySetInnerHTML={{ __html: this.props.page }}/>
 					<div id="revivalSettings" dangerouslySetInnerHTML={{ __html: this.props.revivalSettings }}/>
 					<div id="scripts">
 						{scripts}
