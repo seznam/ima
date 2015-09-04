e2877298a2ec848572a43d5e5b68ae7ae53381a5
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2015-09-04 18:30:53 +0200

    #! divide handleRoute event to preHandleRoute and postHandleRoute events

--- packages/core/client/core/router/events.js
+++ packages/core/client/core/router/events.js
@@ -9,17 +9,29 @@ ns.namespace('Core.Router');
  */
 const EVENTS = Object.freeze({
 	/**
-	 * Router fire event {@code $IMA.$Router.handleRoute} after page manager handle the route.
+	 * Router fire event {@code $IMA.$Router.preHandleRoute} before page manager handle the route.
+	 * Event's data contain {{ params: Object<string, string>}, route: ns.Core.Router.Route,
+	 * path: string}. The {@code path} is current path, the {@code params} are params extracted
+	 * from path and the {@code route} is handle route for path.
+	 *
+	 * @const
+	 * @property PRE_HANDLE_ROUTE
+	 * @type {string}
+	 */
+	PRE_HANDLE_ROUTE: '$IMA.$Router.preHandleRoute',
+
+	/**
+	 * Router fire event {@code $IMA.$Router.postHandleRoute} after page manager handle the route.
 	 * Event's data contain {{response: Object<string, *>, params: Object<string, string>},
 	 * route: ns.Core.Router.Route, path: string}. The {@code response} is page render result.
 	 * The {@code path} is current path, the {@code params} are params extracted from path and
 	 * the {@code route} is handle route for path.
 	 *
 	 * @const
-	 * @property HANDLE_ROUTE
+	 * @property POST_HANDLE_ROUTE
 	 * @type {string}
 	 */
-	HANDLE_ROUTE: '$IMA.$Router.handleRoute'
+	POST_HANDLE_ROUTE: '$IMA.$Router.postHandleRoute'
 });
 
 export default EVENTS;
