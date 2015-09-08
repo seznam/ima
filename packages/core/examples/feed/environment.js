9464395a93491677abcec90794b7bfe98ccc5350
Martin Jurča <martin.jurca@firma.seznam.cz>
2015-09-08 09:45:29 +0200

    clear up the mess in the example enviroment configuration files

--- packages/core/examples/feed/environment.js
+++ packages/core/examples/feed/environment.js
@@ -4,72 +4,85 @@ var version = Math.round(new Date().getTime() / versionCoef) * versionCoef;
 module.exports = (() => {
 
 	return {
+		// The production environment is used as a template for configuration
+		// of other environments
 		prod: {
-			$Debug: false,
-			$Version: version,
+			$Debug: false, // Debug mode
+			$Version: version, // Current server version. Version value is used
+							   // for static assets timestamp.
 			/*
-			 * Pair (key:value) is used for pairing urls with language.
-			 * 
-			 * - Key: 	Url have to start with '//' instead protocol and you can define root path. 
-			 * 			Optional parameter ":language" could be defined in the end, to display language in url.
-			 * 
-			 * - Value: Language definition for url. If ":language" optional parameter is used, 
-			 *			this language is used like default language.
-			 */ 
-			$Language:{
-				'//*.*': 'en'
+			 * Key-value pairs used for configuring the languages used with
+			 * specific hosts or starting paths.
+			 *
+			 * - Key: Has to start with '//' instead of a protocol, and you can
+			 *        define the root path. Optional parameter ":language"
+			 *        could be defined at the end to display language in the
+			 *        URL.
+			 *
+			 * - Value: Language to use when the key is matched by the current
+			 *          URL. If the ":language" parameter is used, the language
+			 *          specified in this value is used as the default language
+			 *          when the path part specifying the language is not
+			 *          present in the current URL.
+			 */
+			$Language: {
+				'//*:*': 'en'
 			},
 			$Server: {
-				port: 3001,
-				staticFolder: '/static',
-				concurrency: 100,
-				clusters: null,
+				port: 3001, // The port at which the server listens for
+							// incoming HTTP connections
+				staticFolder: '/static', // Define the path to the folder with
+										 // static assets
+				concurrency: 100, // The number of application instances (not
+								  // threads) used to handle concurrent
+								  // connections within a single thread
+				clusters: null, // Define the number of server processes you
+								// want to start. Use null for the current
+								// number of available CPU cores.
 				serveSPA: {
-					allow: true,
-					blackList: ['Googlebot', 'SeznamBot']
+					allow: true, // When enabled, and the number of concurrent
+								 // connection exceeds the concurrency, the
+								 // server will serve the application in SPA
+								 // mode (without server-side rendering)
+					blackList: ['Googlebot', 'SeznamBot'] // These user agents
+														  // will always be
+														  // served a
+														  // server-rendered
+														  // page
 				}
 			},
-			$Proxy: {
-				path: '/api',
-				server: 'http://example.com/api/v1'
+			$Proxy: { // Configuration of REST API proxy. All requests to the
+			          // specified path will be proxies to the specified
+			          // server. This proxy should be used only in dev
+			          // environment because of performance a possible security
+			          // concerns.
+				path: '/api', // Path at which the proxy will be listening for
+							  // request
+				server: 'https://example.com/api/v1'
 			}
 		},
+
+		test: {
+			$Language: {
+				'//*:*': 'en'
+			},
+			$Proxy: {
+				server: 'http://example.test/api'
+			}
+		},
+
 		dev: {
 			$Debug: true,
-			/*
-			 * Pair (key:value) is used for pairing urls with language.
-			 * 
-			 * - Key: 	Url have to start with '//' instead protocol and you can define root path. 
-			 * 			Optional parameter ":language" could be defined in the end, to display language in url.
-			 * 
-			 * - Value: Language definition for url. If ":language" optional parameter is used, 
-			 *			this language is used like default language.
-			 */ 
 			$Language: {
-				'//localhost:3001': 'en'
+				'//*:*': 'en'
+			},
+			$Server: {
+				concurrency: 1
 			},
 			$Proxy: {
 				server: 'http://localhost:3001/api'
 			}
-		},
-		test: {
-			$Debug: true,
-			/*
-			 * Pair (key:value) is used for pairing urls with language.
-			 * 
-			 * - Key: 	Url have to start with '//' instead protocol and you can define root path. 
-			 * 			Optional parameter ":language" could be defined in the end, to display language in url.
-			 * 
-			 * - Value: Language definition for url. If ":language" optional parameter is used, 
-			 *			this language is used like default language.
-			 */ 
-			$Language:{
-				'//*:*': 'en'
-			},
-			$Proxy: {
-				server: 'http://api.exmaple.test'
-			}
 		}
 	};
-})();
 
+})();
