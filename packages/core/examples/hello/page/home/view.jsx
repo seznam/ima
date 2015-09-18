483e803d1a56a32bba66aa9dda48ae8768455a00
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2015-09-18 10:40:02 +0200

    #! add attribute target

--- packages/core/examples/hello/page/home/view.jsx
+++ packages/core/examples/hello/page/home/view.jsx
@@ -17,7 +17,7 @@ class View extends ns.Core.Abstract.Component {
 			<div className='l-homepage'>
 				<div className='content'>
 					<img src={this.utils.$Router.getBaseUrl() + this.utils.$Settings.$Static.image + '/imajs-logo.png'} alt='IMA.js logo'/>
-					<h1>{this.utils.$Dictionary.get('home.hello')}, {this.props.message} <a href='//imajs.io' title={this.props.name}>{this.props.name}</a>!</h1>
+					<h1>{this.utils.$Dictionary.get('home.hello')}, {this.props.message} <a href='//imajs.io' title={this.props.name} target='_blank'>{this.props.name}</a>!</h1>
 				</div>
 			</div>
 		);
