b8a1c6e5767c81ac103fb6b4b005f223f5b82adc
Martin Jurča <martin.jurca@firma.seznam.cz>
2015-10-20 11:40:58 +0200

    first public version

---
+++ packages/gulp-task-loader/README.md
@@ -0,0 +1,7 @@
+# IMA.js-gulp-task-loader
+
+This repository houses the build tasks loader for IMA.js applications.
+
+If you are looking for a way to start a new IMA.js application, you should
+follow this link:
+[https://github.com/seznam/IMA.js-skeleton](https://github.com/seznam/IMA.js-skeleton).
---
+++ packages/gulp-task-loader/taskLoader.js
@@ -0,0 +1,40 @@
+
+var fs = require('fs');
+var path = require('path');
+
+/**
+ * Loads the default IMA.js and application-specific gulp tasks. The
+ * application-specific gulp tasks will override the default ones if the names
+ * match.
+ *
+ * @param {Object<string, *>} gulpConfig Configuration of the gulp tasks.
+ */
+module.exports = function (gulpConfig) {
+	loadTasks('./node_modules/IMA.js-gulp-tasks/tasks', gulpConfig);
+	loadTasks('./app/gulp/tasks', gulpConfig);
+};
+
+/**
+ * Loads the gulp tasks defined in the JavaScript files within the specified
+ * directory. The JavaScript files are executed in arbitrary order.
+ *
+ * @param {string} directory The directory containing the JavaScript files
+ *        defining the gulp tasks.
+ * @param {Object<string, *>} gulpConfig Configuration of the gulp tasks.
+ */
+function loadTasks(directory, gulpConfig) {
+	if (!fs.existsSync(directory)) {
+		console.warn('The gulp tasks directory ' + directory + ' does not ' +
+				'exist, skipping');
+		return;
+	}
+
+	fs.readdirSync(directory).filter(function (file) {
+		return file.match(/[.]js$/i);
+	}).forEach(function (file) {
+		var factory = require(directory + path.sep + file);
+		if (factory instanceof Function) {
+			factory(gulpConfig);
+		}
+	});
+}
