4f5348899838d05d8f1b29e25b355dd6f50f1548
Martin Jurča <martin.jurca@firma.seznam.cz>
2015-12-02 12:41:17 +0100

    refactored the IMAError to be more class-like and babel-compatible

---
+++ packages/core/client/core/abstract/imaError.js
@@ -0,0 +1,184 @@
+
+/*
+
+ This file exists only because babel dropped the support for extending native
+ classes :-/
+
+ */
+
+/**
+ * Initializes the error.
+ *
+ * @method constructor
+ * @param {string} message The message describing the cause of the error.
+ * @param {Object<string, *>=} [params={}] A data map providing additional
+ *        details related to the error. It is recommended to set the
+ *        {@code status} field to the HTTP response code that should be sent
+ *        to the client.
+ * @param {boolean=} dropInternalStackFrames Whether or not the call stack
+ *        frames referring to the constructors of the {@codelink IMAError}
+ *        and overriding class(es) should be included in the stack of this
+ *        error.
+ */
+export function constructor(message, params = {},
+		dropInternalStackFrames = true) {
+	/**
+	 * The name of this error, used in the generated stack.
+	 *
+	 * @property name
+	 * @public
+	 * @type {string}
+	 * @default 'IMAError'
+	 */
+	this.name = 'IMAError';
+
+	/**
+	 * The message describing the cause of the error.
+	 *
+	 * @property message
+	 * @type {string}
+	 */
+	this.message = message;
+
+	/**
+	 * A data map providing additional details related to this error.
+	 *
+	 * @property _params
+	 * @private
+	 * @type {Object<string, *>}
+	 */
+	this._params = params;
+
+	/**
+	 * Native error instance we use to generate the call stack. For some reason
+	 * the browsers do not generate call stacks for instances of classes
+	 * extending the native {@codelink Error} class, so we bypass this
+	 * shortcoming like this.
+	 *
+	 * @private
+	 * @property
+	 * @type {Error}
+	 */
+	this._nativeError = new Error(message);
+	this._nativeError.name = this.name;
+
+	// improve compatibility with Gecko
+	if (this._nativeError.columnNumber) {
+		this.lineNumber = this._nativeError.lineNumber;
+		this.columnNumber = this._nativeError.columnNumber;
+		this.fileName = this._nativeError.fileName;
+	}
+
+	/**
+	 * The internal cache of the generated stack. The cache is filled upon
+	 * first access to the {@codelink stack} property.
+	 *
+	 * @private
+	 * @property
+	 * @type {?string}
+	 */
+	this._stack = null;
+
+	/**
+	 * Whether or not the call stack frames referring to the constructors of
+	 * the {@codelink IMAError} and overriding class(es) should be included
+	 * in the stack of this error.
+	 *
+	 * @private
+	 * @property
+	 * @type {boolean}
+	 */
+	this._dropInternalStackFrames = dropInternalStackFrames;
+}
+
+/**
+ * The call stack captured at the moment of creation of this error. The
+ * formatting of the stack is browser-dependant.
+ *
+ * @override
+ * @public
+ * @method getStack
+ * @return {string}
+ */
+export function getStack() {
+	if (this._stack) {
+		return this._stack;
+	}
+
+	var stack = this._nativeError.stack;
+	if (typeof stack !== 'string') {
+		return undefined;
+	}
+
+	// drop the stack trace frames referring to the custom error constructors
+	if (this._dropInternalStackFrames) {
+		var stackLines = stack.split('\n');
+
+		var inheritanceDepth = 1;
+		var currentPrototype = Object.getPrototypeOf(this);
+		while (currentPrototype !== IMAError.prototype) {
+			currentPrototype = Object.getPrototypeOf(currentPrototype);
+			inheritanceDepth++;
+		}
+		stackLines.splice(1, inheritanceDepth);
+
+		this._stack = stackLines.join('\n');
+	} else {
+		this._stack = stack;
+	}
+
+	return this._stack;
+}
+
+/**
+ * Returns the HTTP status to send to the client.
+ *
+ * This method is a shorthand for the following code snippet:
+ * {@code this.getParams().status || 500}.
+ *
+ * @inheritDoc
+ * @override
+ * @method getHttpStatus
+ * @return {number}
+ */
+export function getHttpStatus() {
+	return this._params.status || 500;
+}
+
+/**
+ * Returns the error parameters providing additional details about the error.
+ * The structure of the returned object is always situation dependent, but
+ * the returned object usually contains the {@code status: number} field
+ * which represents the HTTP status to send to the client.
+ *
+ * @override
+ * @method getParams
+ * @return {Object<string, *>} The error parameters providing additional
+ *         details about the error.
+ */
+export function getParams() {
+	return this._params;
+}
+
+/**
+ * Returns the name of this error. The name briefly describes this error.
+ *
+ * @override
+ * @method getName
+ * @return {string} The name of this error.
+ */
+export function getName() {
+	return this.name;
+}
+
+/**
+ * Returns a string representing this error. The string will consist of the
+ * error name and message.
+ *
+ * @override
+ * @method toString
+ * @return {string} A string representing this error.
+ */
+export function toString() {
+	return `${this.name}: ${this.message}`;
+}
--- packages/core/client/core/imaError.js
+++ packages/core/client/core/imaError.js
@@ -1,6 +1,18 @@
 import ns from 'imajs/client/core/namespace';
+import * as errorImplementation from 'imajs/client/core/abstract/imaError';
+
+/*
+
+ This file exists only because babel dropped the support for extending native
+ classes :-/
+
+ */
 
 ns.namespace('Core');
+
+// extend the Error class
+IMAError.prototype = Object.create(Error.prototype);
+IMAError.prototype.constructor = IMAError;
 
 /**
  * Implementation of the Core.Interface.Error interface, providing more
@@ -11,188 +23,97 @@ ns.namespace('Core');
  * @implements Core.Interface.Error
  * @namespace Core
  * @module Core
+ *
+ * @param {string} message The message describing the cause of the error.
+ * @param {Object<string, *>} [params={}] A data map providing additional
+ *        details related to the error. It is recommended to set the
+ *        {@code status} field to the HTTP response code that should be sent
+ *        to the client.
+ * @param {boolean=} dropInternalStackFrames Whether or not the call stack
+ *        frames referring to the constructors of the {@codelink IMAError}
+ *        and overriding class(es) should be included in the stack of this
+ *        error.
  */
-export default class IMAError {
+export default function IMAError(message, params = {},
+		dropInternalStackFrames = true) {
+	Error.call(this, message); // super-call
 
-	/**
-	 * Initializes the error.
-	 *
-	 * @method constructor
-	 * @constructor
-	 * @param {string} message The message describing the cause of the error.
-	 * @param {Object<string, *>} [params={}] A data map providing additional
-	 *        details related to the error. It is recommended to set the
-	 *        {@code status} field to the HTTP response code that should be sent
-	 *        to the client.
-	 * @param {boolean=} dropInternalStackFrames Whether or not the call stack
-	 *        frames referring to the constructors of the {@codelink IMAError}
-	 *        and overriding class(es) should be included in the stack of this
-	 *        error.
-	 */
-	constructor(message, params = {}, dropInternalStackFrames = true) {
-		//super(message);
-		this.constructor.prototype.__proto__ = Error.prototype;
-
-		/**
-		 * The name of this error, used in the generated stack.
-		 *
-		 * @property name
-		 * @public
-		 * @type {string}
-		 * @default 'IMAError'
-		 */
-		this.name = 'IMAError';
-
-		/**
-		 * The message describing the cause of the error.
-		 *
-		 * @property message
-		 * @type {string}
-		 */
-		this.message = message;
-
-		/**
-		 * A data map providing additional details related to this error.
-		 *
-		 * @property _params
-		 * @private
-		 * @type {Object<string, *>}
-		 */
-		this._params = params;
-
-		/**
-		 * Native error instance we use to generate the call stack. For some reason
-		 * the browsers do not generate call stacks for instances of classes
-		 * extending the native {@codelink Error} class, so we bypass this
-		 * shortcoming like this.
-		 *
-		 * @private
-		 * @property
-		 * @type {Error}
-		 */
-		this._nativeError = new Error(message);
-		this._nativeError.name = this.name;
-
-		// improve compatibility with Gecko
-		if (this._nativeError.columnNumber) {
-			this.lineNumber = this._nativeError.lineNumber;
-			this.columnNumber = this._nativeError.columnNumber;
-			this.fileName = this._nativeError.fileName;
-		}
-
-		/**
-		 * The internal cache of the generated stack. The cache is filled upon
-		 * first access to the {@codelink stack} property.
-		 *
-		 * @private
-		 * @property
-		 * @type {?string}
-		 */
-		this._stack = null;
-
-		/**
-		 * Whether or not the call stack frames referring to the constructors of
-		 * the {@codelink IMAError} and overriding class(es) should be included
-		 * in the stack of this error.
-		 *
-		 * @private
-		 * @property
-		 * @type {boolean}
-		 */
-		this._dropInternalStackFrames = dropInternalStackFrames;
-	}
-
-	/**
-	 * The call stack captured at the moment of creation of this error. The
-	 * formatting of the stack is browser-dependant.
-	 *
-	 * @override
-	 * @public
-	 * @property stack
-	 * @type {string}
-	 */
-	get stack() {
-		if (this._stack) {
-			return this._stack;
-		}
-
-		var stack = this._nativeError.stack;
-		if (typeof stack !== 'string') {
-			return undefined;
-		}
-
-		// drop the stack trace frames referring to the custom error constructors
-		if (this._dropInternalStackFrames) {
-			var stackLines = stack.split('\n');
-
-			var inheritanceDepth = 1;
-			var currentPrototype = Object.getPrototypeOf(this);
-			while (currentPrototype !== IMAError.prototype) {
-				currentPrototype = Object.getPrototypeOf(currentPrototype);
-				inheritanceDepth++;
-			}
-			stackLines.splice(1, inheritanceDepth);
-
-			this._stack = stackLines.join('\n');
-		} else {
-			this._stack = stack;
-		}
-
-		return this._stack;
-	}
-
-	/**
-	 * Returns the HTTP status to send to the client.
-	 *
-	 * This method is a shorthand for the following code snippet:
-	 * {@code this.getParams().status || 500}.
-	 *
-	 * @inheritDoc
-	 * @override
-	 * @method getHttpStatus
-	 * @return {number}
-	 */
-	getHttpStatus() {
-		return this._params.status || 500;
-	}
-
-	/**
-	 * Returns the error parameters providing additional details about the error.
-	 * The structure of the returned object is always situation dependent, but
-	 * the returned object usually contains the {@code status: number} field
-	 * which represents the HTTP status to send to the client.
-	 *
-	 * @override
-	 * @method getParams
-	 * @return {Object<string, *>} The error parameters providing additional
-	 *         details about the error.
-	 */
-	getParams() {
-		return this._params;
-	}
-
-	/**
-	 * Returns the name of this error. The name briefly describes this error.
-	 *
-	 * @override
-	 * @method getName
-	 * @return {string} The name of this error.
-	 */
-	getName() {
-		return this.name;
-	}
-
-	/**
-	 * Returns a string representing this error. The string will consist of the
-	 * error name and message.
-	 *
-	 * @override
-	 * @method toString
-	 * @return {string} A string representing this error.
-	 */
-	toString() {
-		return `${this.name}: ${this.message}`;
-	}
+	errorImplementation.constructor.call(
+		this,
+		message,
+		params,
+		dropInternalStackFrames
+	);
 }
+
+/**
+ * The call stack captured at the moment of creation of this error. The
+ * formatting of the stack is browser-dependant.
+ *
+ * @override
+ * @public
+ * @property stack
+ * @type {string}
+ */
+Object.defineProperty(IMAError.prototype, "stack", {
+	configurable: false,
+	enumerable: true,
+	get: function () {
+		return errorImplementation.getStack.call(this);
+	}
+});
+
+/**
+ * Returns the HTTP status to send to the client.
+ *
+ * This method is a shorthand for the following code snippet:
+ * {@code this.getParams().status || 500}.
+ *
+ * @inheritDoc
+ * @override
+ * @method getHttpStatus
+ * @return {number}
+ */
+IMAError.prototype.getHttpStatus = function () {
+	return errorImplementation.getHttpStatus.call(this);
+};
+
+/**
+ * Returns the error parameters providing additional details about the error.
+ * The structure of the returned object is always situation dependent, but
+ * the returned object usually contains the {@code status: number} field
+ * which represents the HTTP status to send to the client.
+ *
+ * @override
+ * @method getParams
+ * @return {Object<string, *>} The error parameters providing additional
+ *         details about the error.
+ */
+IMAError.prototype.getParams = function () {
+	return errorImplementation.getParams.call(this);
+};
+
+/**
+ * Returns the name of this error. The name briefly describes this error.
+ *
+ * @override
+ * @method getName
+ * @return {string} The name of this error.
+ */
+IMAError.prototype.getName = function () {
+	return errorImplementation.getName.call(this);
+};
+
+/**
+ * Returns a string representing this error. The string will consist of the
+ * error name and message.
+ *
+ * @override
+ * @method toString
+ * @return {string} A string representing this error.
+ */
+IMAError.prototype.toString = function () {
+	return errorImplementation.toString.call(this);
+};
 
 ns.Core.IMAError = IMAError;
