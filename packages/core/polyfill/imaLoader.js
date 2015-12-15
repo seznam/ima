90f87e2b9b58e440e5f9fc1740b5f1aa99173a39
Martin Jurča <martin.jurca@firma.seznam.cz>
2015-12-15 10:49:11 +0100

    asynchronous module loader

--- packages/core/polyfill/imaLoader.js
+++ packages/core/polyfill/imaLoader.js
@@ -1,36 +1,69 @@
 (function() {
-	var root = (typeof window !== 'undefined' && window !== null) ? window : GLOBAL;
+	var root;
+	if ((typeof window !== 'undefined') && (window !== null)) {
+		root = window;
+	} else {
+		root = GLOBAL;
+	}
 
 	root.$IMA = root.$IMA || {};
+	var modules = {};
 
 	root.$IMA.Loader = {
-		modules: {},
-		register: function(moduleName, dependencies, makeModule) {
-			var self = this;
-			this.modules[moduleName] = {};
-
-			var module = makeModule(function(key, value) {
-				self.modules[moduleName][key] = value;
-			});
-
-			module.setters.map(function(setter, index) {
-				if (!self.modules[dependencies[index]]) {
-					throw new Error('$IMA.Loader.register: Dependency ' + dependencies[index] +
-							' is not registered. Update your build.js');
-				}
-
-				setter(self.modules[dependencies[index]]);
-			});
-
-			module.execute();
+		modules: modules,
+		register: function (moduleName, dependencies, moduleFactory) {
+			this.modules[moduleName] = {
+				dependencies: dependencies,
+				factory: moduleFactory,
+				instance: null
+			};
 		},
-		import: function(moduleName) {
+		import: function (moduleName) {
 			if (!this.modules[moduleName]) {
-				throw new Error('$IMA.Loader.import: Module name ' + moduleName +
-						' is not registered. Update your build.js.');
+				throw new Error('$IMA.Loader.import: Module name ' +
+						moduleName + ' is not registered. Update your ' +
+						'build.js.');
 			}
 
-			return Promise.resolve(this.modules[moduleName]);
+			return Promise.resolve(resolveModule(moduleName));
+		},
+		initAllModules: function () {
+			Object.keys(modules).forEach(function (moduleName) {
+				resolveModule(moduleName);
+			});
 		}
 	};
+
+	function resolveModule(moduleName, dependencyOf) {
+		if (!modules[moduleName]) {
+			throw new Error('$IMA.Loader.import: Module name ' +
+				moduleName + (
+					dependencyOf ?
+						(' (dependency of ' + dependencyOf + ')') :
+						''
+				) + ' is not registered. Update your build.js.');
+		}
+
+		var module = modules[moduleName];
+		if (module.instance) {
+			return module.instance;
+		}
+
+		var moduleInstance = {};
+		var moduleInitializer = module.factory(function _export(key, value) {
+			moduleInstance[key] = value;
+		});
+		module.instance = moduleInstance; // allow lazy circular dependencies
+
+		var dependencies = module.dependencies.map(function (dependencyName) {
+			return resolveModule(dependencyName, moduleName);
+		});
+		dependencies.forEach(function (dependency, index) {
+			moduleInitializer.setters[index](dependency);
+		});
+
+		moduleInitializer.execute();
+
+		return moduleInstance;
+	}
 })();
