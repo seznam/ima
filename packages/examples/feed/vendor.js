d8ac7ddb8def8295baf4c62dcf509eb17a9591bd
Martin Jurča <martin.jurca@firma.seznam.cz>
2016-02-08 08:17:58 +0100

    made the build script configurations more ES6-like

--- packages/examples/feed/vendor.js
+++ packages/examples/feed/vendor.js
@@ -1,19 +1,15 @@
-var vendorApp = vendor || new Map(); // jshint ignore:line
-
 var react = require('react');
 var reactDOM = require('react-dom');
 var reactDOMServer = require('react-dom/server.js');
 var superAgent = require('superagent');
 
-// use the vendorApp.set(...) method to export
-// libraries from the app/vendor module
-vendorApp.set('React', react);
-vendorApp.set('ReactDOM', reactDOM);
-vendorApp.set('ReactDOMServer', reactDOMServer);
-vendorApp.set('SuperAgent', superAgent);
+// use vendor.set(...) to export libraries from the app/vendor module
+vendor.set('React', react);
+vendor.set('ReactDOM', reactDOM);
+vendor.set('ReactDOMServer', reactDOMServer);
+vendor.set('SuperAgent', superAgent);
 
 /*
-var thirdPartyLibrary = require('thirdPartLibrary');
-
-vendorApp.set('ThirdPartyLibrary', thirdPartLibrary);
-*/
+ var thirdPartyLibrary = require('thirdPartLibrary');
+ vendorApp.set('ThirdPartyLibrary', thirdPartLibrary);
+ */
