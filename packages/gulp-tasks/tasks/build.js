f3f6acf23a014bc62cb64a58f99ec11734bf5c50
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2016-02-25 16:17:05 +0100

    #! remove deleted task copycopy:imajsServer

--- packages/gulp-tasks/tasks/build.js
+++ packages/gulp-tasks/tasks/build.js
@@ -4,7 +4,7 @@ var runSequence = require('run-sequence');
 
 gulp.task('server:build', function (callback) {
 	return runSequence(
-		['copy:imajsServer', 'copy:environment'],
+		'copy:environment',
 		'Es6ToEs5:server',
 		'server:restart',
 		'server:reload',
