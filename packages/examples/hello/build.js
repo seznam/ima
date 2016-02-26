19f453d9c80b49faa4664ab46ea647cf2caf7125
Martin Jurča <martin.jurca@firma.seznam.cz>
2016-02-26 15:11:40 +0100

    updated the build configuration for the hello wolrd example

--- packages/examples/hello/build.js
+++ packages/examples/hello/build.js
@@ -1,40 +1,53 @@
-module.exports = (() => {
+var mainjs = ['./app/main.js'];
 
-	var js = [
-		'./app/!(assets)/**/*.{js,jsx}'
-	];
+var js = [
+	'./app/!(assets)/**/*.{js,jsx}'
+];
 
-	var less = [
-		'./app/assets/less/app.less',
-		'./app/assets/less/setting.less',
-		'./app/assets/less/base.less',
-		'./app/assets/less/layout.less',
-		'./app/assets/less/main.less',
-		'./app/component/**/*.less',
-		'./app/page/**/*.less'
-	];
+var less = [
+	'./app/assets/less/app.less',
+	'./app/assets/less/setting.less',
+	'./app/assets/less/base.less',
+	'./app/assets/less/layout.less',
+	'./app/assets/less/main.less',
+	'./app/component/**/*.less',
+	'./app/page/**/*.less'
+];
 
-	var languages = [
-		'cs',
-		'en'
-	];
+var languages = [
+	'cs',
+	'en'
+];
 
-	var bundle = {
-		js: [
-			'./build/static/js/polyfill.js',
-			'./build/static/js/shim.js',
-			'./build/static/js/vendor.client.js',
-			'./build/static/js/app.client.js'
-		],
-		css: [
-			'./build/static/css/app.css'
-		]
-	};
+var vendors = {
+	common: [
+	],
 
-	return {
-		js: js,
-		less: less,
-		languages: languages,
-		bundle: bundle
-	};
-})();
+	server: [
+	],
+
+	client: [
+	]
+};
+
+var bundle = {
+	js: [
+		'./build/static/js/polyfill.js',
+		'./build/static/js/shim.js',
+		'./build/static/js/vendor.client.js',
+		'./build/static/js/ima.client.js',
+		'./build/static/js/app.client.js'
+	],
+	css: [
+		'./build/static/css/app.css'
+	]
+};
+
+module.exports = {
+	js,
+	mainjs,
+	less,
+	languages,
+	vendors,
+	bundle
+};
