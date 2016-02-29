b393546866c17f8c240ce852fcf003dab95fdfa3
Martin Jurča <martin.jurca@firma.seznam.cz>
2016-02-29 09:23:11 +0100

    build process fixed for ima.js 0.12.0 for the todos example

--- packages/examples/todos/build.js
+++ packages/examples/todos/build.js
@@ -1,45 +1,50 @@
-module.exports = (() => {
+var mainjs = ['./app/main.js'];
 
-	var js = [
-		'./app/config/*.js',
-		'./app/base/*.js',
-		'./app/model/**/*.js',
-		'./app/component/**/*.js',
-		'./app/component/**/*.jsx',
-		'./app/page/*.js',
-		'./app/page/*.jsx',
-		'./app/page/**/*.js',
-		'./app/page/**/*.jsx'
-	];
+var js = [
+	'./app/!(assets)/**/*.{js,jsx}'
+];
 
-	var less = [
-		'./app/assets/less/app.less',
-		'./app/assets/bower/todomvc-common/base.css',
-		'./app/assets/bower/todomvc-app-css/index.css',
-		'./app/component/**/*.less',
-		'./app/page/**/*.less'
-	];
+var less = [
+	'./app/assets/less/app.less',
+	'./app/assets/bower/todomvc-common/base.css',
+	'./app/assets/bower/todomvc-app-css/index.css',
+	'./app/component/**/*.less',
+	'./app/page/**/*.less'
+];
 
-	var languages = [
-		'en'
-	];
+var languages = [
+	'en'
+];
 
-	var bundle = {
-		js: [
-			'./build/static/js/polyfill.js',
-			'./build/static/js/shim.js',
-			'./build/static/js/vendor.client.js',
-			'./build/static/js/app.client.js'
-		],
-		css: [
-			'./build/static/css/app.css'
-		]
-	};
+var vendors = {
+	common: [
+	],
 
-	return {
-		js: js,
-		less: less,
-		languages: languages,
-		bundle: bundle
-	};
-})();
+	server: [
+	],
+
+	client: [
+	]
+};
+
+var bundle = {
+	js: [
+		'./build/static/js/polyfill.js',
+		'./build/static/js/shim.js',
+		'./build/static/js/vendor.client.js',
+		'./build/static/js/ima.client.js',
+		'./build/static/js/app.client.js'
+	],
+	css: [
+		'./build/static/css/app.css'
+	]
+};
+
+module.exports = {
+	js,
+	mainjs,
+	less,
+	languages,
+	vendors,
+	bundle
+};
