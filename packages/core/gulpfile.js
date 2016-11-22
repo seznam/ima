84aed65153a2f9cfda1b8f6f296fa13d1544756a
Martin Jurča <martin.jurca@firma.seznam.cz>
2016-11-22 10:58:28 +0100

    switched to gulp 4, stabilized dev dependencies, updated the build & publish process

--- packages/core/gulpfile.js
+++ packages/core/gulpfile.js
@@ -5,66 +5,73 @@ let babel = require('gulp-babel');
 let plumber = require('gulp-plumber');
 let tap = require('gulp-tap');
 
-gulp.task('compile', () => {
-	return (
-		gulp
-			.src([
-				__dirname + '/main.js',
-				__dirname + '/namespace.js',
-				__dirname + '/Bootstrap.js',
-				__dirname + '/ObjectContainer.js',
-				__dirname + '/vendorLinker.js',
-				__dirname + '/!(node_modules|polyfill)/**/!(*Spec).js'
-			])
-			.pipe(plumber())
-			.pipe(babel({
-				moduleIds: true,
-				presets: ['es2015'],
-				plugins: ['external-helpers-2']
-			}))
-			.pipe(plumber.stop())
-			.pipe(tap((file) => {
-				let moduleName = 'ima' + file.path.slice(__dirname.length, -3);
-				let fileContents = file.contents.toString();
+exports.build = gulp.series(
+	clean,
+	gulp.parallel(
+		compile,
+		copy
+	)
+);
 
-				let dependencyMatcher = /require\(['"]([^'"]+)['"]\)/g;
-				let dependencies = [];
-				let match = dependencyMatcher.exec(fileContents);
-				while (match) {
-					dependencies.push(match[1]);
-					match = dependencyMatcher.exec(fileContents);
-				}
+function compile() {
+	return gulp
+		.src([
+			__dirname + '/main.js',
+			__dirname + '/namespace.js',
+			__dirname + '/Bootstrap.js',
+			__dirname + '/ObjectContainer.js',
+			__dirname + '/vendorLinker.js',
+			__dirname + '/!(node_modules|polyfill)/**/!(*Spec).js'
+		])
+		.pipe(plumber())
+		.pipe(babel({
+			moduleIds: true,
+			plugins: ['transform-es2015-modules-commonjs']
+		}))
+		.pipe(plumber.stop())
+		.pipe(tap((file) => {
+			let moduleName = 'ima' + file.path.slice(__dirname.length, -3);
+			let fileContents = file.contents.toString();
 
-				let exportMatcher = /\nexports\.([a-zA-Z_][a-zA-Z_1-9]*)\s*=\s*([^;]+);/g;
-				let moduleExports = [];
+			let dependencyMatcher = /require\(['"]([^'"]+)['"]\)/g;
+			let dependencies = [];
+			let match = dependencyMatcher.exec(fileContents);
+			while (match) {
+				dependencies.push(match[1]);
+				match = dependencyMatcher.exec(fileContents);
+			}
+
+			let exportMatcher = /\nexports\.([a-zA-Z_][a-zA-Z_1-9]*)\s*=\s*([^;]+);/g;
+			let moduleExports = [];
+			match = exportMatcher.exec(fileContents);
+			while (match) {
+				moduleExports.push({ symbol: match[1], value: match[2] });
 				match = exportMatcher.exec(fileContents);
-				while (match) {
-					moduleExports.push({ symbol: match[1], value: match[2] });
-					match = exportMatcher.exec(fileContents);
-				}
+			}
 
-				// Note: there is no point in specifying the dependencies in
-				// the the IMA loader's register() method, as these will
-				// already be resolved by browserify's require() function used
-				// in the code before with the IMA loader.
-				file.contents = new Buffer(
-					fileContents + '\n\n' +
-					`$IMA.Loader.register('${moduleName}', [], function (_export, _context) {\n` +
-					`	'use strict';\n` +
-					`	return {\n` +
-					`		setters: [],\n` +
-					`		execute: function () {\n` +
-					moduleExports.map(({ symbol, value }) => `			_export('${symbol}', exports.${symbol});\n`).join('') +
-					`		}\n` +
-					`	};\n` +
-					`});\n`
-				);
-			}))
-			.pipe(gulp.dest(__dirname + '/dist'))
-	);
-});
+			// Note: there is no point in specifying the dependencies in
+			// the the IMA loader's register() method, as these will
+			// already be resolved by browserify's require() function used
+			// in the code before with the IMA loader.
+			file.contents = new Buffer(
+				fileContents + '\n\n' +
+				`$IMA.Loader.register('${moduleName}', [], function (_export, _context) {\n` +
+				`	'use strict';\n` +
+				`	return {\n` +
+				`		setters: [],\n` +
+				`		execute: function () {\n` +
+				moduleExports.map(
+					({ symbol, value }) => `			_export('${symbol}', exports.${symbol});\n`)
+				.join('') +
+				`		}\n` +
+				`	};\n` +
+				`});\n`
+			);
+		}))
+		.pipe(gulp.dest(__dirname + '/dist'));
+}
 
-gulp.task('copy-metadata', () => {
+function copy() {
 	return gulp
 		.src([
 			__dirname + '/polyfill/*.js',
@@ -76,10 +83,8 @@ gulp.task('copy-metadata', () => {
 			__dirname + '/test.js'
 		])
 		.pipe(gulp.dest(__dirname + '/dist'));
-});
+}
 
-gulp.task('clean', () => {
-	return del([
-		__dirname + '/dist'
-	]);
-});
+function clean() {
+	return del(__dirname + '/dist');
+}
