8b38249d2b06e37baac588c894b5f63e303fc8cf
Ondřej Baše <ondrej.base@firma.seznam.cz>
2017-02-07 10:37:54 +0100

    #79 Response cookie tests

--- packages/core/router/__tests__/ResponseSpec.js
+++ packages/core/router/__tests__/ResponseSpec.js
@@ -8,7 +8,16 @@ describe('ima.router.Response', () => {
 		response = new Response();
 	});
 
-	it('should be 1', () => {
-		expect(1).toEqual(1);
+	it('should convert cookie maxAge to ms for Express', () => {
+		let options = { maxAge: 1 };
+		response._prepareCookieOptionsForExpress(options);
+		expect(options.maxAge).toEqual(1000);
+	});
+
+	it('should remove cookie maxAge: null for Express', () => {
+		// Because Express converts null to 0, which is not intended.
+		let options = { maxAge: null };
+		response._prepareCookieOptionsForExpress(options);
+		expect(options.maxAge).toBeUndefined();
 	});
 });
