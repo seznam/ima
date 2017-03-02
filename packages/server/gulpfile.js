5c3d5dfff49a445dce700273d1bb078fa9d79f32
Martin Jurča <martin.jurca@firma.seznam.cz>
2017-03-02 12:38:00 +0100

    switched to gulp 4 and npm 4

--- packages/server/gulpfile.js
+++ packages/server/gulpfile.js
@@ -1,17 +1,13 @@
-var gulp = require('gulp');
-var plumber = require('gulp-plumber');
-var jasmine = require('gulp-jasmine');
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const jasmine = require('gulp-jasmine');
 
-//run test
-gulp.task('test', () => {
-	return (
-		gulp.src(['./tests/**/*.js', './tests/*.js'])
-			.pipe(jasmine({ includeStackTrace: false }))
-	);
-});
+exports.test = test;
+function test() {
+	return gulp
+		.src(['./tests/**/*.js', './tests/*.js'])
+		.pipe(jasmine({ includeStackTrace: false }));
+}
 
-
-// -------------------------------------PRIVATE HELPER TASKS
-gulp.task('dev', function() {
-	gulp.watch(['./lib/**/*.js', './lib/*.js', './tests/**/*.js', './tests/*.js'], ['test']);
-});
+exports.dev = () => gulp
+	.watch(['./lib/**/*.js', './lib/*.js', './tests/**/*.js', './tests/*.js'], test);
