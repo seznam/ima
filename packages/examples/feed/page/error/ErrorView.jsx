20f6e7dd87bd71ce61095db7d49946af14374fc0
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2017-05-06 19:44:58 +0200

    #! The Proptypes is imported from prop-types package

--- packages/examples/feed/page/error/ErrorView.jsx
+++ packages/examples/feed/page/error/ErrorView.jsx
@@ -1,5 +1,6 @@
 import ns from 'ima/namespace';
-import React, { PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 import AbstractComponent from 'ima/page/AbstractComponent';
 
 ns.namespace('app.page.error');
--- packages/examples/feed/page/notFound/NotFoundView.jsx
+++ packages/examples/feed/page/notFound/NotFoundView.jsx
@@ -1,5 +1,6 @@
 import AbstractComponent from 'ima/page/AbstractComponent';
-import React, { PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 
 class NotFoundView extends AbstractComponent {
 
