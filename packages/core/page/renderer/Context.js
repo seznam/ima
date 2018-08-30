c4cfcc9648dbc2fb3575cc814dcc097461d30d4a
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2018-08-30 13:58:52 +0200

    #context: improve usage new React context API

--- packages/core/page/renderer/Context.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createNamedContext } from 'consume-multiple-contexts';
-
-const context = React.createContext();
-
-const Utils = createNamedContext('$Utils', context);
-const { Consumer, Provider } = context;
-
-export { Utils, Consumer, Provider };
