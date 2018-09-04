f74ed9a94293a88ad0463d02e9cef1638af20999
Vojtěch Šimko <vojtech.simko@firma.seznam.cz>
2018-09-04 13:29:35 +0200

    Updated JSDoc

--- packages/core/page/handler/PageHandler.js
+++ packages/core/page/handler/PageHandler.js
@@ -10,8 +10,14 @@ export default class PageHandler {
    *        be managed.
    * @param {ManagedPage} managedPage The currently managed page - soon-to-be
    *        previously managed page.
-   * @param {{ type: string, event: Event, url: string }} [action] An action
-   *        object describing what triggered the routing.
+   * @param {{ 
+   *          type: string,
+   *          event: Event,
+   *          url: string,
+   *          options: Object,
+   *          route: Route,
+   *          params: Object
+   *        }} [action] An action object describing what triggered the routing.
    */
   handlePreManagedState() {}
 
@@ -22,8 +28,14 @@ export default class PageHandler {
    * @param {ManagedPage} previousManagedPage The data of the page that was
    *        previously managed.
    * @param {ManagedPage} managedPage The currently managed page.
-   * @param {{ type: string, event: Event, url: string }} [action] An action
-   *        object describing what triggered the routing.
+   * @param {{ 
+   *          type: string,
+   *          event: Event,
+   *          url: string,
+   *          options: Object,
+   *          route: Route,
+   *          params: Object
+   *        }} [action] An action object describing what triggered the routing.
    */
   handlePostManagedState() {}
 }
