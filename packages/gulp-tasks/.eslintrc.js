0032cd39b6a9085ab5a0cd0d6abd1a7188935f72
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2019-01-01 22:43:27 +0100

    #! fixed code style

--- packages/gulp-tasks/.eslintrc.js
+++ packages/gulp-tasks/.eslintrc.js
@@ -1,35 +1,35 @@
 module.exports = {
-	'extends': ['eslint:recommended', 'prettier'],
-	'parser': 'babel-eslint',
-	'rules': {
-		'prettier/prettier': [
-			'error', {
-				singleQuote: true,
-				semi: true,
-				jsxBracketSameLine: true
-			}
-		],
+  'extends': ['eslint:recommended', 'prettier'],
+  'parser': 'babel-eslint',
+  'rules': {
+    'prettier/prettier': [
+      'error', {
+        singleQuote: true,
+        semi: true,
+        jsxBracketSameLine: true
+      }
+    ],
 
-		'no-console': ['error', {
-			allow: ['warn', 'error', 'info']
-		}]
+    'no-console': ['error', {
+      allow: ['warn', 'error', 'info']
+    }]
 
-	},
-	'plugins': [
-		'prettier'
-	],
-	'settings': {
-		'ecmascript': 2016
-	},
-	'parserOptions': {
-		'sourceType': 'module',
-		'ecmaVersion': 6,
-	},
-	'env': {
-		'browser': true,
-		'node': true,
-		'es6': true
-	},
-	'globals': {
-	}
+  },
+  'plugins': [
+    'prettier'
+  ],
+  'settings': {
+    'ecmascript': 2016
+  },
+  'parserOptions': {
+    'sourceType': 'module',
+    'ecmaVersion': 6,
+  },
+  'env': {
+    'browser': true,
+    'node': true,
+    'es6': true
+  },
+  'globals': {
+  }
 };
