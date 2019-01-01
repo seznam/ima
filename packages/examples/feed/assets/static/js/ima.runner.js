cb1cb429fada51618d6f8cfe9048e85dcb53ac33
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2019-01-01 22:07:29 +0100

    #! fixed code style

--- packages/examples/feed/assets/static/js/ima.runner.js
+++ packages/examples/feed/assets/static/js/ima.runner.js
@@ -1,5 +1,5 @@
 (function() {
-  var root = typeof window !== 'undefined' && window !== null ? window : GLOBAL;
+  var root = typeof window !== 'undefined' && window !== null ? window : GLOBAL; // eslint-disable-line
 
   root.$IMA.Runner.run();
 })();
