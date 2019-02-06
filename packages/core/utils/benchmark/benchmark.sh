2260c75e98978995bddfe442cb8859c666578a4a
Filip Satek <filip.satek@firma.seznam.cz>
2019-02-06 08:43:07 +0100

    Test against app:feed instead of app:hello; Fix display results; moved all dev dependencies to the root folder

--- packages/core/utils/benchmark/benchmark.sh
+++ packages/core/utils/benchmark/benchmark.sh
@@ -13,16 +13,12 @@ PACKAGE_VERSION=`cat package.json | grep \"version\" | cut -d':' -f2 | cut -d'"'
 PACKAGE_NAME=`cat package.json | grep \"name\" | head -1 | cut -d':' -f2 | cut -d'"' -f2`
 
 # Setup local registry
-cd "$ROOT_DIR/utils/benchmark"
-npm install
-node_modules/.bin/verdaccio -l "$NPM_LOCAL_REGISTRY_URL_NO_PROTOCOL" -c verdaccio_config.yml >/dev/null &
+node_modules/.bin/verdaccio -l "$NPM_LOCAL_REGISTRY_URL_NO_PROTOCOL" -c utils/benchmark/verdaccio_config.yml >/dev/null &
 NPM_LOCAL_REGISTRY_PID=$!
 
 npm config set "//$NPM_LOCAL_REGISTRY_URL_NO_PROTOCOL/:_authToken" "0"
 
 # Release ima-core
-cd "$ROOT_DIR"
-npm install --registry="$NPM_LOCAL_REGISTRY_URL"
 npm run build
 cd dist
 sed -i "s/\"version\":\s\".*\"/\"version\": \"$PACKAGE_VERSION\"/" package.json
@@ -30,12 +26,11 @@ sed -i "s#https://registry.npmjs.org/#${NPM_LOCAL_REGISTRY_URL}#" package.json
 npm publish
 
 # Setup IMA.js-skeleton
-cd "$ROOT_DIR"
 git clone "$SKELETON_URL" ima-skeleton
 cd ima-skeleton
 sed -i "s/\"$PACKAGE_NAME\":\s\".*\"/\"$PACKAGE_NAME\": \"$PACKAGE_VERSION\"/" package.json
 npm install --registry="$NPM_LOCAL_REGISTRY_URL"
-npm run app:hello
+npm run app:feed
 npm run build
 mv build/ima/config/environment.js build/ima/config/environment.orig.js
 mv "$ROOT_DIR/utils/benchmark/app/environment.js" build/ima/config/environment.js
@@ -45,8 +40,8 @@ IMA_SKELETON_SERVER_PID=$!
 sleep 5
 
 # Run test
-cd "$ROOT_DIR/utils/benchmark"
-node_modules/.bin/autocannon -c $PARALLEL_TEST_CONNECTIONS -n "$TARGET_WEB_URL"
+cd "$ROOT_DIR"
+node_modules/.bin/autocannon -c $PARALLEL_TEST_CONNECTIONS --no-progress "$TARGET_WEB_URL"
 
 # Cleanup
 kill $NPM_LOCAL_REGISTRY_PID
