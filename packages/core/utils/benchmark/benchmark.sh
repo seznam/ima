d483c7484367af43c0508db503dcf43c409ca7eb
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2019-03-22 19:56:28 +0100

    ci(benchmark): debug

--- packages/core/utils/benchmark/benchmark.sh
+++ packages/core/utils/benchmark/benchmark.sh
@@ -22,6 +22,7 @@ npm config set "//$NPM_LOCAL_REGISTRY_URL_NO_PROTOCOL/:_authToken" "0"
 # Release ima-core
 npm run build
 cd dist
+ls -l
 sed -i "" "s/\"version\": \"$PACKAGE_VERSION_OLD\"/\"version\": \"$PACKAGE_VERSION\"/" package.json
 sed -i "" "s#https://registry.npmjs.org/#${NPM_LOCAL_REGISTRY_URL}#" package.json
 npm publish
