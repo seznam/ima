61e31a185e8a59d103182b819823a4b6365d6de2
Vojtěch Šimko <SimkoVojta@gmail.com>
2019-08-16 12:41:39 +0200

    feat(page-renderer): component utils lazy creation of instances
    Creating instances of utility classes only when they are requested via `getUtils()` method.

--- packages/core/page/renderer/ComponentUtils.js
+++ packages/core/page/renderer/ComponentUtils.js
@@ -1,63 +1,67 @@
-
 export default class ComponentUtils {
-
+  /**
+   * Initializes the registry used for managing component utils.
+   *
+   * @param {ObjectContainer} oc The application's dependency injector - the
+   *        object container.
+   */
+  constructor(oc) {
     /**
-     * Initializes the registry used for managing component utils.
+     * The application's dependency injector - the object container.
      *
-     * @param {ObjectContainer} oc The application's dependency injector - the
-     *        object container.
+     * @type {ObjectContainer}
      */
-    constructor(oc) {
-        /**
-         * The application's dependency injector - the object container.
-         *
-         * @type {ObjectContainer}
-         */
-        this._oc = oc;
-
-        /**
-         * Map of registered utilities.
-         * 
-         * @type {Object<string, Object>}
-         */
-        this._utilities = {};
-    }
+    this._oc = oc;
 
     /**
-     * Registers single utility class or multiple classes in alias->class mapping.
-     * 
-     * @param {function(new: Object)|Object<string, function(new: Object)>} componentUtilityClass
-     * @param {string|null} alias
-     * @returns {Object|Object<string, Object>}
+     * Map of registered utilities.
+     *
+     * @type {Object<string, Object>}
      */
-    register(componentUtilityClass, alias = null) {
-        if (typeof componentUtilityClass === 'function') {
+    this._utilities = {};
+  }
 
-            const utilityInstance = this._oc.get(componentUtilityClass);
-            this._utilities[alias || componentUtilityClass.name] = utilityInstance;
-
-            return utilityInstance;
-
-        } else if (typeof componentUtilityClass === 'object') {
-            const utilityInstances = {};
-
-            for (const alias of Object.keys(componentUtilityClass)) {
-                const instance = this._oc.get(componentUtilityClass);
-
-                this._utilities[alias] = instance;
-                utilityInstances[alias] = instance;
-            }
-
-            return utilityInstances;
+  /**
+   * Registers single utility class or multiple classes in alias->class mapping.
+   *
+   * @param {function(new: Object)|Object<string, function(new: Object)>} componentUtilityClass
+   * @param {string|null} alias
+   * @returns {Object|Object<string, Object>}
+   */
+  register(componentUtilityClass, alias = null) {
+    if (typeof componentUtilityClass === 'function') {
+      this._utilities[
+        alias || componentUtilityClass.name
+      ] = componentUtilityClass;
+    } else if (typeof componentUtilityClass === 'object') {
+      for (const alias of Object.keys(componentUtilityClass)) {
+        if (!componentUtilityClass.hasOwnProperty(alias)) {
+          continue;
         }
+
+        this._utilities[alias] = componentUtilityClass[alias];
+      }
+    }
+  }
+
+  /**
+   * Returns object containing all registered utilities
+   *
+   * @returns {Object<string, Object>}
+   */
+  getUtils() {
+    if (this._oc.has('$Utils')) {
+      // fallback for backward compatibility
+      return this._oc.get('$Utils');
     }
 
-    /**
-     * Returns object containing all registered utilities
-     * 
-     * @returns {Object<string, Object>}
-     */
-    getUtils() {
-        return this._utilities;
+    const utilities = {};
+
+    // create instance of each utility class
+    for (const utilityAlias of Object.keys(this._utilities)) {
+      utilities[utilityAlias] = this._oc.get(this._utilities[utilityAlias]);
     }
+
+    return utilities;
+  }
 }
