47244ad67d3dbd35f1855fc993332910d3e694ff
Filip Satek <filip.satek@BRN>
2019-11-07 14:07:54 +0100

    refactor: 💡 cleanup

--- utils/benchmark/benchmark.sh
+++ utils/benchmark/benchmark.sh
@@ -21,7 +21,7 @@ NPM_LOCAL_REGISTRY_PID=$!
 npm config set "//$NPM_LOCAL_REGISTRY_URL_NO_PROTOCOL/:_authToken" "0"
 
 # Release ima-core
-cd "$CORE_DIR" # dist should be already generated from lerna bootstrap
+cd "$CORE_DIR"
 sed -i "s#\"version\":\s\".*\"#\"version\": \"$PACKAGE_VERSION\"#" package.json
 sed -i "s#https://registry.npmjs.org/#${NPM_LOCAL_REGISTRY_URL}#" package.json
 npm publish
