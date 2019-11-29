6f485e59475ab513282cea96b740d0335dc86043
Jan Šimeček <simecekjann@gmail.com>
2019-11-29 10:21:52 +0100

    feat: added dependency check to create-ima-app (#13)

--- packages/create-ima-app/bin/create-ima-app.js
+++ packages/create-ima-app/bin/create-ima-app.js
@@ -5,6 +5,12 @@ const execa = require('execa');
 const chalk = require('chalk');
 const argv = require('yargs').argv;
 
+const { checkNodeVersion } = require('../utils/utils');
+const { error } = require('../utils/printUtils');
+
+const MIN_NODE_VERSION = 10;
+const MAX_NODE_VERSION = 12;
+
 if (argv._.length === 0) {
   // eslint-disable-next-line no-console
   console.log(`
@@ -13,6 +19,20 @@ Please specify your new project directory:
 
 For example:
   ${chalk.blue('create-ima-app')} ${chalk.green('my-ima-application')}`);
+
+  process.exit(0);
+}
+
+if (!checkNodeVersion(MIN_NODE_VERSION, MAX_NODE_VERSION)) {
+  // eslint-disable-next-line no-console
+  error(
+    `You are currently using ${chalk.bold(
+      'unsupported version'
+    )} of NodeJS (${chalk.bold(process.version)}).
+       Please updated NodeJS to the latest supported major version: ${chalk.bold(
+         `v${MAX_NODE_VERSION}.x.x`
+       )}.`
+  );
 
   process.exit(0);
 }
