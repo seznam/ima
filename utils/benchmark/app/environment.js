a750900db85556a3c5f390472f3a80524a13c6d6
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2020-03-02 20:41:56 +0100

    ci: 🎡 turn off overload server detection

--- utils/benchmark/app/environment.js
+++ utils/benchmark/app/environment.js
@@ -1,6 +1,7 @@
 const environment = require('./environment.orig.js');
 
 environment.prod.$Server.clusters = 1;
+environment.prod.$Server.overloadConcurrency = undefined;
 environment.prod.$Server.serveSPA.allow = false;
 environment.prod.$Server.cache.enabled = false;
 
