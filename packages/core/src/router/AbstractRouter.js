131fb7fdb4d239c1efe49b9254ca1585e1b90315
Vojtěch Šimko <SimkoVojta@gmail.com>
2021-07-13 23:41:12 +0200

    fix: 🐛 passing original params to not-found and error route (#122)
    * fix: 🐛 passing original params to not-found and error route
    
    When not-found or error route is handled instead of original route all
    former params were lost. This caused trouble when params contained
    application-wide info that was required even for the not-found or error
    page.
    
    * test: 💍 updated tests to count with persistent route params
    
    * style: 💄 fixed lint errors
    
    * refactor: 💡 dedicated method for adding params to error route
    
    Co-authored-by: Simko, Vojtech <vojtech.simko@firma.seznam.cz>

--- packages/core/src/router/AbstractRouter.js
+++ packages/core/src/router/AbstractRouter.js
@@ -310,9 +310,9 @@ export default class AbstractRouter extends Router {
    * @inheritdoc
    */
   async handleError(params, options = {}, locals = {}) {
-    let routeError = this._routeHandlers.get(RouteNames.ERROR);
+    let errorRoute = this._routeHandlers.get(RouteNames.ERROR);
 
-    if (!routeError) {
+    if (!errorRoute) {
       let error = new GenericError(
         `ima.core.router.AbstractRouter:handleError cannot process the ` +
           `error because no error page route has been configured. Add ` +
@@ -323,33 +323,35 @@ export default class AbstractRouter extends Router {
       return Promise.reject(error);
     }
 
+    params = this._addParamsFromOriginalRoute(params);
+
     const action = {
       url: this.getUrl(),
       type: ActionTypes.ERROR
     };
 
     locals.action = action;
-    locals.route = routeError;
+    locals.route = errorRoute;
 
     await this._runMiddlewares(
       [
         ...this._getMiddlewaresForRoute(RouteNames.ERROR),
-        ...routeError.getOptions().middlewares
+        ...errorRoute.getOptions().middlewares
       ],
       params,
       locals
     );
 
-    return this._handle(routeError, params, options, action);
+    return this._handle(errorRoute, params, options, action);
   }
 
   /**
    * @inheritdoc
    */
   async handleNotFound(params, options = {}, locals = {}) {
-    let routeNotFound = this._routeHandlers.get(RouteNames.NOT_FOUND);
+    let notFoundRoute = this._routeHandlers.get(RouteNames.NOT_FOUND);
 
-    if (!routeNotFound) {
+    if (!notFoundRoute) {
       let error = new GenericError(
         `ima.core.router.AbstractRouter:handleNotFound cannot processes ` +
           `a non-matching route because no not found page route has ` +
@@ -361,24 +363,26 @@ export default class AbstractRouter extends Router {
       return Promise.reject(error);
     }
 
+    params = this._addParamsFromOriginalRoute(params);
+
     const action = {
       url: this.getUrl(),
       type: ActionTypes.ERROR
     };
 
     locals.action = action;
-    locals.route = routeNotFound;
+    locals.route = notFoundRoute;
 
     await this._runMiddlewares(
       [
         ...this._getMiddlewaresForRoute(RouteNames.NOT_FOUND),
-        ...routeNotFound.getOptions().middlewares
+        ...notFoundRoute.getOptions().middlewares
       ],
       params,
       locals
     );
 
-    return this._handle(routeNotFound, params, options, action);
+    return this._handle(notFoundRoute, params, options, action);
   }
 
   /**
@@ -562,5 +566,25 @@ export default class AbstractRouter extends Router {
     for (const middleware of middlewares) {
       await middleware.run(params, locals);
     }
+  }
+
+  /**
+   * Obtains original route that was handled before not-found / error route
+   * and assigns its params to current params
+   *
+   * @param {Object<string, string>} params Route params for not-found or
+   *        error page
+   * @returns {Object<string, string>} Provided params merged with params
+   *        from original route
+   */
+  _addParamsFromOriginalRoute(params) {
+    const originalPath = this._getCurrentlyRoutedPath();
+    const { route } = this._getRouteHandlersByPath(originalPath);
+
+    if (!route) {
+      return params;
+    }
+
+    return Object.assign({}, route.extractParameters(originalPath), params);
   }
 }
