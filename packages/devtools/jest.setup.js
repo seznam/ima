fa928de400767c94d74d344a59c7f871e2d68d2b
Jiří Fencl <jiri.fencl@kancelar.seznam.cz>
2021-10-04 09:34:21 +0200

    feat: 🎸 Added support for node16, npm7

--- packages/devtools/jest.setup.js
+++ packages/devtools/jest.setup.js
@@ -1,4 +1,13 @@
 const enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 
+global.chrome = {
+  storage: {
+    local: {
+      get: () => {},
+      set: () => {}
+    }
+  }
+};
+
 enzyme.configure({ adapter: new Adapter() });
