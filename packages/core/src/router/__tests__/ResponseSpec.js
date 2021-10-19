a5b97939d9fbb9cc9549b4989450f624d2b2b47e
Anna Frankova (corvidism) <frankova@corvidism.com>
2021-10-19 15:10:19 +0200

    test: 💍 Fix and add new tests

--- packages/core/src/router/__tests__/ResponseSpec.js
+++ packages/core/src/router/__tests__/ResponseSpec.js
@@ -21,4 +21,30 @@ describe('ima.core.router.Response', () => {
     expect(options.maxAge).toEqual(null);
     expect(expressOptions.maxAge).toBeUndefined();
   });
+
+  describe('redirect', () => {
+    it('should set cookies, headers, and redirect', () => {
+      response._response = {
+        cookie: jest.fn(),
+        redirect: jest.fn(),
+        set: jest.fn()
+      };
+      response._internalCookieStorage = new Map([
+        ['key1', 'value1'],
+        ['key2', 'value2'],
+        ['key3', 'value3']
+      ]);
+
+      const url = 'some/url/or/other';
+      const headers = {
+        'Custom-header': 'Some value'
+      };
+
+      response.redirect(url, 303, headers);
+
+      expect(response._response.set).toHaveBeenCalledWith(headers);
+      expect(response._response.cookie).toHaveBeenCalledTimes(3);
+      expect(response._response.redirect).toHaveBeenCalledWith(303, url);
+    });
+  });
 });
