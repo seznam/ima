5c40f13ab5ea57a6d849d622223547fa2c7c620d
Anna Frankova (corvidism) <frankova@corvidism.com>
2022-02-11 09:56:13 +0100

    chore: Set up Jest to test ts files
    The `ts-jest` and `typescript` versions are matched to the current Jest version.

--- packages/core/jest.conf.js
+++ packages/core/jest.conf.js
@@ -2,6 +2,11 @@ const defaultConfig = require('../../jest.conf.js');
 
 module.exports = {
   ...defaultConfig,
+  transform: {
+    ...defaultConfig.transform,
+    '^.+\\.tsx?$': 'ts-jest',
+    '\\.[jt]sx?$': 'babel-jest',
+  },
   setupFiles: [
     './test.js',
     './polyfill/imaLoader.js',
