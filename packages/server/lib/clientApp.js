a76f1cc291da7e1c927180ee51e5ec44f67f0755
Jan Šimeček <simecekjann@gmail.com>
2022-03-13 22:08:31 +0100

    fix: 🐛 Fixed lengthy server error processing

--- packages/server/lib/clientApp.js
+++ packages/server/lib/clientApp.js
@@ -1,18 +1,9 @@
 const path = require('path');
 const fs = require('fs');
 const ejs = require('ejs');
-const stackTrace = require('stack-trace');
-const asyncEach = require('async-each');
-const hljs = require('highlight.js');
-const sep = require('path').sep;
 const instanceRecycler = require('./instanceRecycler.js');
 const errorToJSON = require('error-to-json').default;
 const Cache = require('./cache.js').Cache;
-
-hljs.configure({
-  tabReplace: '  ',
-  lineNodes: true,
-});
 
 module.exports = (environment, logger, languageLoader, appFactory) => {
   const app = appFactory();
@@ -37,87 +28,26 @@ module.exports = (environment, logger, languageLoader, appFactory) => {
   );
 
   async function _displayDetails(err, req, res) {
-    let callstack = stackTrace.parse(err);
-    let fileIndex = 1;
+    res.status(500);
 
-    asyncEach(
-      callstack,
-      (stackFrame, cb) => {
-        // exclude core node modules and node modules
-        if (
-          !stackFrame.fileName ||
-          !stackFrame.fileName.includes(sep) ||
-          /node_modules/.test(stackFrame.fileName) ||
-          /internal/.test(stackFrame.fileName)
-        ) {
-          return cb();
-        }
+    if (!err || !err?.stack) {
+      logger.error('Failed to display error page', {
+        error: errorToJSON(err),
+      });
 
-        fs.readFile(stackFrame.fileName, 'utf-8', (err, content) => {
-          if (err) {
-            return cb(err);
-          }
-
-          content = hljs.highlight(content, { language: 'javascript' });
-
-          // start a few lines before the error or at the beginning of
-          // the file
-          let start = Math.max(stackFrame.lineNumber - 11, 0);
-          let lines = content.value
-            .split('\n')
-            .map(line => `<span class="line">${line}</span>`);
-          // end a few lines after the error or the last line of the file
-          let end = Math.min(stackFrame.lineNumber + 10, lines.length);
-          let snippet = lines.slice(start, end);
-          // array starts at 0 but lines numbers begin with 1, so we have
-          // to subtract 1 to get the error line position in the array
-          let errLine = stackFrame.lineNumber - start - 1;
-
-          snippet[errLine] = snippet[errLine].replace(
-            '<span class="line">',
-            '<span class="line error-line">'
-          );
-
-          stackFrame.content = snippet.join('\n');
-          stackFrame.errLine = errLine;
-          stackFrame.startLine = start;
-          stackFrame.id = 'file-' + fileIndex;
-
-          fileIndex++;
-
-          cb(null, stackFrame);
-        });
-      },
-      async (error, callstack) => {
-        if (!Array.isArray(callstack)) {
-          callstack = [];
-        }
-
-        callstack = callstack.filter(item => !!item);
-
-        res.status(500);
-
-        // if something bad happened while processing the stacktrace make
-        // sure to return something useful
-        if (error) {
-          logger.error('Failed to display error page', {
-            error: errorToJSON(error),
-          });
-          res.send(err.stack);
-        } else {
-          res.send(
-            errorTemplate({
-              devServerPublic: process.env.IMA_CLI_DEV_SERVER_PUBLIC_URL,
-              serverError: {
-                name: err.name,
-                message: err.message,
-                stack: err.stack.toString(),
-              },
-            })
-          );
-        }
-      }
-    );
+      res.send(err.stack);
+    } else {
+      res.send(
+        errorTemplate({
+          devServerPublic: process.env.IMA_CLI_DEV_SERVER_PUBLIC_URL,
+          serverError: {
+            name: err.name,
+            message: err.message,
+            stack: err.stack.toString(),
+          },
+        })
+      );
+    }
   }
 
   function _initApp(req, res, appMain) {
@@ -410,6 +340,7 @@ module.exports = (environment, logger, languageLoader, appFactory) => {
         status: 500,
         error: error,
       });
+
       _displayDetails(error, req, res);
     } else {
       let appPromise = Promise.resolve(app);
