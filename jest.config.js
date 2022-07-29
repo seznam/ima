ae0a1b1992f5d222f856d4421abb76e1accd0502
Filip Satek <filip.satek@firma.seznam.cz>
2022-07-29 15:02:09 +0200

    chore: use native scripts comands with cache instead of turbo

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,3 @@
 module.exports = {
-  bail: false,
-  verbose: true,
-  testEnvironment: 'node',
-  modulePaths: ['<rootDir>/'],
-  testRegex: '(/__tests__/).*Spec\\.jsx?$',
-  transform: {
-    '\\.[jt]sx?$': [
-      'babel-jest',
-      {
-        plugins: ['@babel/plugin-transform-modules-commonjs'],
-      },
-    ],
-  },
+  projects: ['<rootDir>/packages/*/jest.config.js'],
 };
