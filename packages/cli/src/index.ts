627c6d40ec41004b715a2491a23cc4a36fb32ca9
Jan Šimeček <simecekjann@gmail.com>
2022-08-21 18:19:03 +0200

    Added changeset

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,12 +1,5 @@
 export { AmpPluginOptions, AmpPlugin } from './plugins/amp/AmpPlugin';
 
-export {
-  ScrambleCssPluginOptions,
-  ScrambleCssPlugin,
-} from './plugins/scramble-css/ScrambleCssPlugin';
-
-export { ScrambleCssMinimizer } from './plugins/scramble-css/plugin/ScrambleCssMinimizer';
-
 export { PostCssPlugin } from './webpack/plugins/PostCssPlugin';
 
 export { createLogger } from './lib/logger';
