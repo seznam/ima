5c387a7786e5984fe98c9404175fe183b783c004
Jan Šimeček <simecekjann@gmail.com>
2022-08-30 09:21:55 +0200

    Update config.ts

--- packages/cli/src/webpack/config.ts
+++ packages/cli/src/webpack/config.ts
@@ -473,8 +473,8 @@ export default async (
         },
         {
           /**
-           * Allows the use of #!if / #!else / #!elseif / #!endif directives
-           * on client` server, ctx === 'client'|'client.es'|'server' variables
+           * Allows the use of // @if | @else | @elseif | @endif directives
+           * on client server, ctx === 'client'|'client.es'|'server' variables
            * to conditionally exclude parts of the source code for concrete bundles.
            */
           test: /\.(js|mjs|jsx|cjs|ts|tsx)$/,
