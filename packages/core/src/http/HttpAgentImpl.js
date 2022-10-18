97ae0a14eaa50867cbf75324234602a8e5da6da9
Jan Šimeček <simecekjann@gmail.com>
2022-10-18 13:37:45 +0200

    Fixed cleaning of http responses of abort controller data

--- packages/core/src/http/HttpAgentImpl.js
+++ packages/core/src/http/HttpAgentImpl.js
@@ -292,10 +292,13 @@ export default class HttpAgentImpl extends HttpAgent {
   _request(method, url, data, options) {
     const cacheKey = this.getCacheKey(method, url, data);
 
-    const cachePromise = this._proxy.request(method, url, data, options).then(
-      response => this._proxyResolved(response),
-      error => this._proxyRejected(error)
-    );
+    const cachePromise = this._proxy
+      .request(method, url, data, options)
+      .then(
+        response => this._proxyResolved(response),
+        error => this._proxyRejected(error)
+      )
+      .then(this._cleanCacheResponse);
 
     this._internalCacheOfPromises.set(cacheKey, cachePromise);
 
@@ -464,16 +467,6 @@ export default class HttpAgentImpl extends HttpAgent {
    * @param {HttpAgent~Response} agentResponse The response of the server.
    */
   _saveAgentResponseToCache(agentResponse) {
-    /**
-     * Create copy of agentResponse without AbortController and AbortController signal.
-     * Setting agentResponse with AbortController or signal into cache would result in crash.
-     */
-    const { signal, ...fetchOptions } =
-      agentResponse.params.options.fetchOptions || {};
-    const { abortController, ...options } = agentResponse.params.options || {};
-
-    options.fetchOptions = fetchOptions;
-
     const cacheKey = this.getCacheKey(
       agentResponse.params.method,
       agentResponse.params.url,
@@ -484,13 +477,31 @@ export default class HttpAgentImpl extends HttpAgent {
 
     this._cache.set(
       cacheKey,
-      {
-        ...agentResponse,
-        params: { ...agentResponse.params, options },
-      },
+      this._cleanCacheResponse(agentResponse),
       agentResponse.params.options.ttl
     );
 
     agentResponse.cached = false;
+  }
+
+  /**
+   * Cleans cache response from data (abort controller), that cannot be persisted,
+   * before saving the data to the cache.
+   */
+  _cleanCacheResponse(response) {
+    /**
+     * Create copy of agentResponse without AbortController and AbortController signal.
+     * Setting agentResponse with AbortController or signal into cache would result in crash.
+     */
+    // eslint-disable-next-line no-unused-vars
+    const { signal, ...fetchOptions } =
+      response.params.options.fetchOptions || {};
+    const { abortController, ...options } = response.params.options || {};
+    options.fetchOptions = fetchOptions;
+
+    return {
+      ...response,
+      params: { ...response.params, options },
+    };
   }
 }
