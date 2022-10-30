b32c4fb855db279c0628ed1012bbf754589f91d4
Jan Šimeček <simecekjann@gmail.com>
2022-10-30 23:08:33 +0100

    Removed unused code from ima-plugin config

--- packages/react-page-renderer/ima-plugin.config.js
+++ packages/react-page-renderer/ima-plugin.config.js
@@ -28,23 +28,3 @@ module.exports = [
     plugins: [],
   },
 ];
-
-// const config = generateConfig(true);
-
-// const exclude = ['**/server.ts/**'];
-
-// config.forEach(_config => {
-//   if (_config.exclude) {
-//     _config.exclude.push(...exclude);
-//   } else {
-//     _config.exclude = exclude;
-//   }
-// });
-
-// const serverConfig = createBaseConfig('commonjs');
-
-// serverConfig.input = './hook';
-// serverConfig.output = './dist/hook';
-// serverConfig.plugins = [];
-
-// config.push(serverConfig);
