e6f5274e495e32557aee8df110a443a12a09a95c
Jan Šimeček <simecekjann@gmail.com>
2022-11-17 13:21:49 +0100

    Fixed memstatic path filter

--- packages/server/lib/factory/memStaticProxyMiddlewareFactory.js
+++ packages/server/lib/factory/memStaticProxyMiddlewareFactory.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function memStaticProxyMiddlewareFactory() {
   if (process.env.IMA_CLI_WATCH && !process.env.IMA_CLI_WRITE_TO_DISK) {
     return createProxyMiddleware(
-      pathname => !pathname.startsWith('/static/js'),
+      pathname => !pathname.startsWith('/static/js/'),
       {
         logLevel: 'silent',
         target: process.env.IMA_CLI_DEV_SERVER_PUBLIC_URL,
