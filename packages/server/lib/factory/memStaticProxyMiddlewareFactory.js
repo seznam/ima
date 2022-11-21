05d369626cd1094fda48a422f03bf86d74f2d528
Jan Šimeček <simecekjann@gmail.com>
2022-11-21 18:09:02 +0100

    Added support for custom publicPath for memStaticProxy

--- packages/server/lib/factory/memStaticProxyMiddlewareFactory.js
+++ packages/server/lib/factory/memStaticProxyMiddlewareFactory.js
@@ -2,13 +2,13 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function memStaticProxyMiddlewareFactory() {
   if (process.env.IMA_CLI_WATCH && !process.env.IMA_CLI_WRITE_TO_DISK) {
-    return createProxyMiddleware(
-      pathname => !pathname.startsWith('/static/js/'),
-      {
-        logLevel: 'silent',
-        target: process.env.IMA_CLI_DEV_SERVER_PUBLIC_URL,
-      }
-    );
+    return createProxyMiddleware(pathname => !pathname.includes('static/js/'), {
+      logLevel: 'silent',
+      target: process.env.IMA_CLI_DEV_SERVER_PUBLIC_URL,
+      ...(process.env.IMA_CLI_PUBLIC_PATH
+        ? { pathRewrite: { [`^${process.env.IMA_CLI_PUBLIC_PATH}`]: '/' } }
+        : {}),
+    });
   }
 
   return (req, res, next) => next();
