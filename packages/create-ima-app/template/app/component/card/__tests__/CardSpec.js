dfbef82dec04c48746ba0e718882812ca41a0119
Lukas Panchartek <lukas.panchartek@Lukas-MacBook-Pro.local>
2022-12-21 09:54:42 +0100

    #cnsqa-993 create-ima-app unit test

--- packages/create-ima-app/template/app/component/card/__tests__/CardSpec.js
+++ packages/create-ima-app/template/app/component/card/__tests__/CardSpec.js
@@ -1,5 +1,23 @@
+import Card from '../Card';
+import { shallow } from 'enzyme';
+
 describe('Card', () => {
   it('can render', () => {
-    expect(1).toBe(1);
+    const title = 'Test card';
+    const href = 'https://www.seznam.cz';
+    const children = 'Some content of the card.';
+
+    const wrapper = shallow(Card({ children, title, href }));
+
+    expect(wrapper.hasClass('card')).toBe(true);
+
+    const cardTitle = wrapper.find('a');
+    const cardContent = wrapper.find('p');
+
+    expect(cardTitle.props().href).toEqual(href);
+    expect(cardTitle.find('h3').text()).toContain(title);
+    expect(cardContent.props().dangerouslySetInnerHTML.__html).toEqual(
+      children
+    );
   });
 });
