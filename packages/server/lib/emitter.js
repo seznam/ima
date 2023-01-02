b71bb4a2b7ae05408e9e8bc8c9ed368fad2cd9d4
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2023-01-02 12:45:09 +0100

    chore: update esmj/emitter to 0.2.0 (#354)

--- packages/server/lib/emitter.js
+++ packages/server/lib/emitter.js
@@ -1,19 +1,16 @@
-const { Emitter, Event, createEvent } = require('@esmj/emitter');
+const { Emitter, createEvent } = require('@esmj/emitter');
 
-const IMAEvent = {
-  ...Event,
-
+const Event = {
   BeforeError: 'ima.server.beforeError',
+  Error: 'ima.server.error',
   AfterError: 'ima.server.afterError',
 
   BeforeRequest: 'ima.server.beforeRequest',
   Request: 'ima.server.request',
   AfterRequest: 'ima.server.afterRequest',
-  RequestError: 'ima.server.requestError',
   BeforeResponse: 'ima.server.beforeResponse',
   Response: 'ima.server.response',
   AfterResponse: 'ima.server.afterResponse',
-
   CreateBootConfig: 'ima.server.bootConfig',
   CreateImaApp: 'ima.server.createApp',
 };
@@ -21,5 +18,5 @@ const IMAEvent = {
 module.exports = {
   Emitter,
   createEvent,
-  Event: IMAEvent,
+  Event,
 };
