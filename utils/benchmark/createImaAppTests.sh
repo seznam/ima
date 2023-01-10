b5a64e75eb2cbea1b1b7a2081f00d564ed5e31d1
Lukas Panchartek <lukas.panchartek@Lukas-MacBook-Pro-2.local>
2023-01-10 12:32:31 +0100

    #cnsqa-993 new ima-app path in scripts

--- utils/benchmark/createImaAppTests.sh
+++ utils/benchmark/createImaAppTests.sh
@@ -2,9 +2,13 @@
 
 echo "Run test benchmark on create-ima-app"
 cd "$ROOT_DIR_IMA"
+echo '=============pwd============'
+echo pwd
 node_modules/.bin/autocannon -c $PARALLEL_TEST_CONNECTIONS --no-progress "$TARGET_WEB_URL"
 
 cd "$ROOT_DIR_IMA_APP"
+echo '=============pwd============'
+echo pwd
 
 echo "Run lint for create-ima-app"
 npm run lint
