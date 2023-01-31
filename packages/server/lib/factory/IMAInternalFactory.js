ca55af922398615fe916954bbf5489005b469e22
Filipoliko <satek.filip@gmail.com>
2023-01-31 17:45:53 +0100

    Revert "Revert "fix: use res.locals instead of res.""

--- packages/server/lib/factory/IMAInternalFactory.js
+++ packages/server/lib/factory/IMAInternalFactory.js
@@ -139,7 +139,7 @@ module.exports = function IMAInternalFactory({
       routeName = routeInfo.route.getName();
     }
 
-    res.$IMA = res.$IMA ? { ...res.$IMA, routeName } : { routeName };
+    res.locals.routeName = routeName;
   }
 
   function _importAppMainSync({ environment, context = {} }) {
