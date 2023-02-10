5714aa5a2e70b4bc33ac87a8aee8e6f5aac4a41e
Jan Šimeček <simecekjann@gmail.com>
2023-02-10 19:39:22 +0100

    Updated eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -151,7 +151,6 @@ module.exports = {
             extensions: ['.js', '.jsx', '.mjs', '.json'],
           },
           typescript: {
-            // alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
             project: 'packages/*/tsconfig.json',
           },
         },
