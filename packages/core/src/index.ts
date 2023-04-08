72309bcad094d307c0ceb61f39711a785999ea3c
Jan Šimeček <simecekjann@gmail.com>
2023-04-08 21:55:41 +0200

    Fixed type export

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -155,9 +155,9 @@ export { ClientWindow } from './window/ClientWindow';
 export { ServerWindow } from './window/ServerWindow';
 export {
   Window,
-  CustomEventTargetMap,
-  EventTargetEventMap,
-  EventTargetTarget,
+  type CustomEventTargetMap,
+  type EventTargetEventMap,
+  type EventTargetTarget,
 } from './window/Window';
 export {
   getInitialImaConfigFunctions,
