f854d70cfafb5b36ee5aea3f6ef89944da5a41b7
Jan Šimeček <simecekjann@gmail.com>
2023-04-10 19:21:11 +0200

    Fixed lint

--- packages/react-page-renderer/src/renderer/__tests__/ServerPageRendererSpec.ts
+++ packages/react-page-renderer/src/renderer/__tests__/ServerPageRendererSpec.ts
@@ -1,16 +1,16 @@
+import type { RouteOptions } from '@ima/core';
 import {
   ControllerDecorator,
   Dispatcher,
   GenericError,
   MetaManager,
+  Settings,
 } from '@ima/core';
-import type { RouteOptions } from '@ima/core';
 import * as Helper from '@ima/helpers';
 import { toMockedInstance } from 'to-mock';
 
 import { AbstractPureComponent } from '../../component/AbstractPureComponent';
 import { BlankManagedRootView } from '../../component/BlankManagedRootView';
-import { Settings } from '../../types';
 import { PageData } from '../AbstractPageRenderer';
 import { PageRendererFactory } from '../PageRendererFactory';
 import { ServerPageRenderer } from '../ServerPageRenderer';
