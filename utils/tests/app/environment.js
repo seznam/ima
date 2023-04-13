d02e18958eb07740914be4070467a734571546f4
Jiří Fencl <jiri.fencl@kancelar.seznam.cz>
2023-04-13 14:26:46 +0200

    test(cia): override protocol for CI integration tests

--- utils/tests/app/environment.js
+++ utils/tests/app/environment.js
@@ -8,4 +8,6 @@ environment.prod.$Server.cache.enabled = false;
 environment.test.$Server.concurrency = 0;
 environment.test.$Server.serveSPA = { allow: true };
 
+environment.test.$Server.protocol = 'https';
+
 module.exports = environment;
