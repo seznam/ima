3ea102910ac2873ad613d5b92cf021a33880c221
jan.kucera <jan.kucera@firma.seznam.cz>
2024-08-01 11:30:26 +0200

    clean

--- packages/core/src/router/StaticRoute.ts
+++ packages/core/src/router/StaticRoute.ts
@@ -318,15 +318,9 @@ export class StaticRoute extends AbstractRoute<string> {
    */
   _replaceOptionalParametersInPath(path: string, optionalParams: string[]) {
     const pattern = optionalParams.reduce((path, paramExpr, idx, matches) => {
-      const lastIdx = matches.length - 1;
       const hasSlash = paramExpr.substr(0, 2) === '\\/';
       const separator = hasSlash ? '/' : '';
-
-      let regExpr = '(?:' + separator + `([^/?]+)(?=/|$))?`;
-
-      if (idx === lastIdx) {
-        regExpr += '';
-      }
+      const regExpr = '(?:' + separator + `([^/?]+)(?=/|$))?`;
 
       return path.replace(paramExpr, regExpr);
     }, path);
