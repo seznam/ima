52ca33be6fd145cc0499e63140daf057d1a9f76a
Filip Satek <filip.satek@firma.seznam.cz>
2024-08-19 13:31:53 +0200

    test: add missing dependency in create-ima-app test

--- utils/tests/testRunner.sh
+++ utils/tests/testRunner.sh
@@ -17,7 +17,7 @@ cd "$ROOT_DIR_IMA"
 
 CREATE_IMA_APP_DIR="$ROOT_DIR_IMA/packages/create-ima-app"
 PACKAGE_VERSION="0.0.0-next"
-PACKAGES="cli core create-ima-app dev-utils error-overlay helpers hmr-client server react-page-renderer"
+PACKAGES="cli core create-ima-app dev-utils error-overlay helpers hmr-client server react-page-renderer testing-library"
 
 # Setup local registry
 node_modules/.bin/verdaccio -l "$NPM_LOCAL_REGISTRY_URL_NO_PROTOCOL" -c utils/tests/verdaccio_config.yml >/dev/null &
