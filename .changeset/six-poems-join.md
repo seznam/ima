5c50ea4b6d38cece3d551bdeff8d8e6534fdec35
Filip Satek <filip.satek@firma.seznam.cz>
2025-02-17 15:52:44 +0100

    fix: compatibility with windows

---
+++ .changeset/six-poems-join.md
@@ -0,0 +1,5 @@
+---
+"@ima/testing-library": patch
+---
+
+Fix Windows compatibility when using this package as a Jest preset
---
+++ packages/testing-library/jest-preset.js
@@ -0,0 +1,3 @@
+// This file is neccessary for Windows users
+// https://github.com/jestjs/jest/issues/15511
+module.exports = require('./dist/cjs/jest-preset.js');
