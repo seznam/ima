778e03570cab4f4cd8d8029cff37b574e76119e0
Jan Šimeček <simecekjann@gmail.com>
2025-06-27 14:05:29 +0200

    Fixed integration test

--- utils/tests/app/environment.js
+++ utils/tests/app/environment.js
@@ -3,15 +3,15 @@ const environment = require('./environment.orig.js');
 environment.prod = {
   ...environment.prod,
   $Server: {
-    ...environment.prod.$Server,
+    ...environment.prod?.$Server,
     clusters: 1,
     overloadConcurrency: undefined,
     serveSPA: {
-      ...environment.prod.$Server.serveSPA,
+      ...environment.prod?.$Server?.serveSPA,
       allow: false,
     },
     cache: {
-      ...environment.prod.$Server.cache,
+      ...environment.prod?.$Server?.cache,
       enabled: false,
     },
   },
