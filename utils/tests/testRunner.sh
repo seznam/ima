6092d33d21f4729474113965333cd50c9c6cf55f
Jan Šimeček <simecekjann@gmail.com>
2025-08-27 12:53:05 +0200

    Fixed test runner script

--- utils/tests/testRunner.sh
+++ utils/tests/testRunner.sh
@@ -13,14 +13,28 @@ cd "$ROOT_DIR_IMA"
 CREATE_IMA_APP_DIR="$ROOT_DIR_IMA/packages/create-ima-app"
 PACKAGES="@ima/cli @ima/core @ima/dev-utils @ima/error-overlay @ima/helpers @ima/hmr-client @ima/server @ima/react-page-renderer @ima/testing-library"
 
-# Pack ima packages to and link
+# Pack ima packages from local filesystem and link
 for PACKAGE in $PACKAGES ; do
     echo "Working on $PACKAGE"
-    npm pack $PACKAGE
-    SANITIZED_PACKAGE_NAME=$(echo "$PACKAGE" | sed 's#@ima/#ima-#g')
-    PACKAGE_PATH="`pwd`/`echo $SANITIZED_PACKAGE_NAME-*.tgz`"
 
+    # Convert package name to directory name (e.g., @ima/cli -> cli)
+    PACKAGE_DIR=$(echo "$PACKAGE" | sed 's#@ima/##g')
+
+    # cd into the package directory
+    cd "$ROOT_DIR_IMA/packages/$PACKAGE_DIR"
+
+    # Pack from local filesystem and specify destination
+    npm pack --pack-destination="$ROOT_DIR_IMA"
+
+    # Get the generated tarball path in the root directory
+    SANITIZED_PACKAGE_NAME=$(echo "$PACKAGE" | sed 's#@ima/#ima-#g')
+    PACKAGE_PATH="$ROOT_DIR_IMA/`echo $SANITIZED_PACKAGE_NAME-*.tgz`"
+
+    # Update the template package.json to use local tarball
     sed -i "s#\"$PACKAGE\":\s\".*\"#\"$PACKAGE\": \"$PACKAGE_PATH\"#" $CREATE_IMA_APP_DIR/template/common/package.json
+
+    # Return to the original directory
+    cd "$ROOT_DIR_IMA"
 done
 
 cd "$ROOT_DIR_IMA"
@@ -52,4 +66,3 @@ npm run build
 
 # Run tests
 source "$ROOT_DIR_IMA/utils/tests/createImaAppTests.sh"
-
