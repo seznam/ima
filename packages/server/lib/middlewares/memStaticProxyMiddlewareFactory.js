704d6fff36c5642a2f3cb12997e3d564ccfd764e
Jan Šimeček <simecekjann@gmail.com>
2025-11-14 11:31:44 +0100

    Fixed issue with mem proxy

--- packages/server/lib/middlewares/memStaticProxyMiddlewareFactory.js
+++ packages/server/lib/middlewares/memStaticProxyMiddlewareFactory.js
@@ -1,12 +1,14 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { legacyCreateProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function memStaticProxyMiddlewareFactory() {
   if (process.env.IMA_CLI_WATCH && !process.env.IMA_CLI_WRITE_TO_DISK) {
-    return createProxyMiddleware({
-      pathFilter: pathname => !pathname.includes('static/js/'),
-      target: process.env.IMA_CLI_DEV_SERVER_PUBLIC_URL,
-      logLevel: 'silent',
-    });
+    return legacyCreateProxyMiddleware(
+      pathname => !pathname.includes('static/js/'),
+      {
+        target: process.env.IMA_CLI_DEV_SERVER_PUBLIC_URL,
+        logLevel: 'silent',
+      }
+    );
   }
 
   return (req, res, next) => next();
