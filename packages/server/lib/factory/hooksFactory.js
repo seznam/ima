7cce3469f0558b0e6bd7d5c9fc40c9623e27da0d
Filip Satek <filip.satek@firma.seznam.cz>
2026-04-20 08:44:44 +0200

    fix: pass request to getThreats for per-request threat caching

--- packages/server/lib/factory/hooksFactory.js
+++ packages/server/lib/factory/hooksFactory.js
@@ -242,7 +242,7 @@ module.exports = function hooksFactory({
         hasReachedMax: instanceRecycler.hasReachedMaxConcurrentRequests(),
       });
 
-      event.threats = event?.performance?.severity.getThreats();
+      event.threats = event?.performance?.severity.getThreats(event.req);
 
       if (_isServerOverloaded(event)) {
         event.context?.timing?.end('hooks.performanceCheck', {
